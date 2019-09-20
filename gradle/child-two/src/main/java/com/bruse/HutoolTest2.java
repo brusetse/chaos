914a54e759ee5ab87c2486b364977c50570e8386
brusetse <brusetse@163.com>
2019-09-20 18:23:33 +0800

    add gradle dir

---
+++ gradle/child-two/src/main/java/com/bruse/HutoolTest2.java
@@ -0,0 +1,13 @@
+package com.bruse;
+
+import cn.hutool.core.date.DateUtil;
+
+import java.util.Date;
+
+public class HutoolTest2 {
+
+    public static void main(String[] args) {
+        Date date = DateUtil.parse("2019-09-19");
+        System.out.println(date);
+    }
+}
